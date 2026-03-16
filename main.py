5e8bed06f740ed945ff410352620cb0f86bb7204
Benignoalcala <134248669+Benignoalcala@users.noreply.github.com>
2026-03-16 03:46:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-
 import os
 import asyncio
-import aiohttp
-from telethon import TelegramClient, events
+from telethon import TelegramClient
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-MAKE_WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-SOURCE_CHANNEL = os.getenv("SOURCE_CHANNEL", "OSINTdefender")
-
-client = TelegramClient("session", API_ID, API_HASH)
-
-@client.on(events.NewMessage(chats=SOURCE_CHANNEL))
-async def handler(event):
-    message = event.message.message
-    if message:
-        async with aiohttp.ClientSession() as session:
-            await session.post(MAKE_WEBHOOK_URL, json={"message": message})
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
 
 async def main():
+    client = TelegramClient("session", api_id, api_hash)
     await client.start()
-    print("Userbot corriendo...")
+
+    print("Userbot conectado a Telegram correctamente")
+
     await client.run_until_disconnected()
 
 asyncio.run(main())
