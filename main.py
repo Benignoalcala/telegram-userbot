421e37205eda5f25cd3021597058ac87b1ccb1f9
Benignoalcala <134248669+Benignoalcala@users.noreply.github.com>
2026-03-17 23:34:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,29 +12,26 @@ WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 
 client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
-@client.on(events.NewMessage)
-async def handler(event):
-    try:
-        message_text = event.raw_text or ""
-        chat = await event.get_chat()
-chat_username = getattr(chat, "username", None)
-print("DEBUG CHAT:", chat_username)
 
-        # FILTRO DE CANALES
-        if not chat_username:
-    return
+@client.on(events.NewMessage)
 async def handler(event):
     try:
         message_text = event.raw_text or ""
         chat = await event.get_chat()
         chat_username = getattr(chat, "username", None)
 
-        message_link = ""
+        print("DEBUG CHAT:", chat_username)
+
+        if not chat_username:
+            return
+
         if chat_username.lower() not in [c.lower() for c in SOURCE_CHANNELS]:
-    return
+            return
+
+        message_link = f"https://t.me/{chat_username}/{event.message.id}"
 
         payload = {
-            "channel": SOURCE_CHANNEL,
+            "channel": chat_username,
             "message_id": event.message.id,
             "text": message_text,
             "date": str(event.message.date),
@@ -47,10 +44,17 @@ async def handler(event):
     except Exception as e:
         print(f"Error enviando mensaje a Make: {e}")
 
+
 async def main():
     await client.start()
+
+    me = await client.get_me()
+    print("USUARIO DEL BOT:", me.username, me.id)
+
     print("Userbot conectado a Telegram correctamente")
     print(f"Escuchando mensajes nuevos de: {SOURCE_CHANNELS}")
+
     await client.run_until_disconnected()
+
 
 asyncio.run(main())
